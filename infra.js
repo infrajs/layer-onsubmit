ab7132fa274ee10ad6aef90b39b9a5cf9f2e6ecb
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-02-27 15:39:28 +0400

    char access

--- infra.js
+++ infra.js
@@ -1,15 +1,21 @@
-if (Controller && Controller.parsedAdd) Controller.parsedAdd(function(layer){//parsed должен забираться после установки msg config-a
-	//После onsubmit слой должен перепарсится
-	if (!layer.onsubmit) return '';
-	if (!layer.config || !layer.config.ans) return '';
-	var str=layer.config.ans.msg;
-	if (!str) str='';
-	if (layer.config.ans.time) {
-		str += layer.config.ans.time;
-	}
-	return str;		
-});
-Event.handler('Layer.onshow', function (layer) {
-	//onsubmit
-	infrajs.setonsubmit(layer);
-});
+(async () => {
+	let Load = (await import('/vendor/akiyatkin/load/Load.js')).default
+	let CDN = await Load.on('import-default', '/vendor/akiyatkin/load/CDN.js')
+	await CDN.load('jquery');
+
+	if (Controller && Controller.parsedAdd) Controller.parsedAdd(function(layer){//parsed должен забираться после установки msg config-a
+		//После onsubmit слой должен перепарсится
+		if (!layer.onsubmit) return '';
+		if (!layer.config || !layer.config.ans) return '';
+		var str=layer.config.ans.msg;
+		if (!str) str='';
+		if (layer.config.ans.time) {
+			str += layer.config.ans.time;
+		}
+		return str;		
+	});
+	Event.handler('Layer.onshow', function (layer) {
+		//onsubmit
+		infrajs.setonsubmit(layer);
+	});
+})();
